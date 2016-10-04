50a518ff8b1ecfa1bceace47229792b78d73e27a
dmitriy <dmitriy@home>
2016-10-03 17:01:45 -0700

    added access control annotation

---
+++ src/java/org/aldan3/annot/Access.java
@@ -0,0 +1,14 @@
+package org.aldan3.annot;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target( { ElementType.TYPE, ElementType.METHOD })
+public @interface Access {
+	String [] roles() default  {};
+
+	// TODO add authorizer attribute 
+}
