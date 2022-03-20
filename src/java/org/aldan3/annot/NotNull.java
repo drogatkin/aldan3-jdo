5113c46976ade8dc4615fb63ed36f9c71f3eb299
dmitriy <jaddressbook@gmail.com>
2022-03-19 21:41:39 -0700

    added experemental no null annotation

---
+++ src/java/org/aldan3/annot/NotNull.java
@@ -0,0 +1,17 @@
+package org.aldan3.annot;
+
+import static java.lang.annotation.ElementType.TYPE_PARAMETER;
+import static java.lang.annotation.ElementType.TYPE_USE;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
+@Documented
+@Retention(RUNTIME)
+@Target({ TYPE_PARAMETER, TYPE_USE })
+//@TypeQualifier
+public @interface NotNull {
+
+}
