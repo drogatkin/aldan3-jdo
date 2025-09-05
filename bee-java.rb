5e11374ae03eba2838799c8689bf3ed6e6cceaba
orangepi <metricstream@gmail.com>
2025-09-05 13:10:11 -0700

    updated to use common script

--- bee-java.rb
+++ bee-java.rb
@@ -9,64 +9,8 @@ build_file ="${project}.jar"
 resources ="${domain}.${project}.resources"
 manifestf =""
 main_class= "${domain}.${project}.Main"
+test class=""
 
-env =../rust_util/src/script/common-java.7b:file
-include(env) # comment out when a standard java installation is used
+comm =..${~/~}simscript${~/~}comm-java.7b:file
 
-target clean {
-    dependency {true}
-    exec rm  (
-        -r,
-        ${build_directory}/${domain},
-        ${build_directory}/${build_file}
-    )
-}
-
-target compile:. {
-   dependency {
-       or {
-              newerthan(${source_directory}/.java,${build_directory}/.class)
-       }
-   }
-   {
-        display(Compiling Java src ...)
-       newerthan(${source_directory}/.java,${build_directory}/.class)
-       assign(main src,~~)
-       exec javac (
-         -d,
-         ${build_directory},
-        -cp,
-         ${build_directory},
-         -source,
-         8,
-         -target,
-         8,
-         main src
-       )     
-      if {
-         neq(${~~}, 0)
-         then {
-            panic("Compilation error(s)")
-         }
-     }
-   }
-}
-
-target jar {
-      dependency {
-         anynewer(${build_directory}/${domain}/*,${build_directory}/${build_file})
-      }
-      dependency {
-          target(compile)
-      }
-     
-     {    display(Jarring ${build_file} ...)
-          exec jar (
-            -cf,
-            ${build_directory}/${build_file},
-            -C,
-            ${build_directory},
-            ${domain}
-          )
-     }
-}
+include(comm)
